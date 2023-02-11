3d0a63f22384e6e012095eb3d730fcc61952cac2
zamar <zerll9100@gmail.com>
2023-02-11 23:33:52 +0900

    4단계 1차원배열 8958 OX문제 진행중

---
+++ 4단계 - 1차원 배열/8958.py
@@ -0,0 +1,45 @@
+# 문제
+# "OOXXOXXOOO"와 같은 OX퀴즈의 결과가 있다. O는 문제를 맞은 것이고, X는 문제를 틀린 것이다.
+# 문제를 맞은 경우 그 문제의 점수는 그 문제까지 연속된 O의 개수가 된다. 예를 들어, 10번 문제의 점수는 3이 된다.
+
+# "OOXXOXXOOO"의 점수는 1+2+0+0+1+0+0+1+2+3 = 10점이다.
+
+# OX퀴즈의 결과가 주어졌을 때, 점수를 구하는 프로그램을 작성하시오.
+
+# 입력
+# 첫째 줄에 테스트 케이스의 개수가 주어진다.
+# 각 테스트 케이스는 한 줄로 이루어져 있고, 길이가 0보다 크고 80보다 작은 문자열이 주어진다.
+# 문자열은 O와 X만으로 이루어져 있다.
+
+# 출력
+# 각 테스트 케이스마다 점수를 출력한다.
+
+# 예제 입력 - 5
+#            OOXXOXXOOO
+#            OOXXOOXXOO
+#            OXOXOXOXOXOXOX
+#            OOOOOOOOOO
+#            OOOOXOOOOXOOOOX
+
+# 예제 출력 - 10
+#            9
+#            7
+#            55
+#            30
+
+# 테스트 케이스 개수
+case_count = input()
+
+# 입력받은 OX정보를 리스트에 담기위한 변수
+ox_list = []
+
+test_str = "OXOXOXO"
+
+# 테스트 케이스 개수만큼 OX를 입력받음.
+# 입력 받은 후 ox_list에 append
+for i in range(int(case_count)):
+    ox = input()
+    ox_list.append(ox)
+    print(ox)
+
+# print(len(test_str))
