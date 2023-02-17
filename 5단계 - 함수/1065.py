218aabbd10a2be4cfcd58c3f4ad9de846e1cdad0
zamar <zerll9100@gmail.com>
2023-02-17 23:37:38 +0900

    5단계 - 함수 1065 한수 [완료하였으나 열린 코드로 변경이 필요함]

--- 5단계 - 함수/1065.py
+++ 5단계 - 함수/1065.py
@@ -1,3 +1,8 @@
+# 구글 검색을 해보면 다들 자릿수를 지정해서 99까지는 count하는 방식에 등차수열의 자릿수를 nums[0] - nums[1] == nums[1] - nums[2]로
+# 체크를 하는 방법으로 다들 하였더라. 일단은 나도 1일 1커밋을 해야하기에 고민하다가 올리기는 하지만
+# 만약에.. 입력 부분의 '첫째 줄에 1000보다 작은 자연수 N이 주어진다'의 요구조건이 변경된다면??
+# 흠.. 좀 더 유연한? 코드를 짜야 할 것 같다.
+
 # 한수
 
 # 문제
@@ -14,11 +19,30 @@
 # 예제 입력 - 110
 # 예제 출력 - 99
 
-def hansu(number):
-    str_number = str(number)
-    len(str_number)
+def check_hansu(strNumber, strNumberLength):
 
-    return 0
+    hansu = 0
+
+    for n in range(1, int(strNumber) + 1):
+
+        # 1부터 99까지는 모두 한수
+        if n <= 99:
+            hansu += 1
+
+        else:
+            # 숫자를 자릿수대로 분리
+            nums = list(map(int, str(n)))
+            # 등차수열 확인 -> 1000보다 작기때문에 가능한 조건..
+            if nums[0] - nums[1] == nums[1] - nums[2]:
+                hansu += 1
+
+    return hansu
 
 
-hansu(int(input()))
+# 입력받은 자연수
+number = input()
+# 자연수의 자리수
+numberLength = len(number)
+
+# 입력받은 자연수, 자연수의 자리수를 파라미터로 넘겨줌
+print(check_hansu(number, numberLength))
